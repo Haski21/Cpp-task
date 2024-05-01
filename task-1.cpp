8af283bbb0812c3bef47cd40df850a54fb51ebdf
Haski21 <81989841+Haski21@users.noreply.github.com>
2024-05-01 19:32:51 +0300

    Add files via upload

---
+++ task-1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int number = 0;
+	unsigned long long int sum = 0;
+
+	cout << "Enter the last number: ";
+	cin >> number;
+
+
+	for (int i = 3; i < number; i++)
+	{
+		if (i % 3 == 0 or i % 5 ==0)
+		{
+			sum += i;
+		}
+	}
+
+	cout << "Sum = " << sum;
+
+}
+
